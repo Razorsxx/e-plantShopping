0b37997774fccf0c08dda7e1ee79e9f31df1a068
Aniel Lorenci <anielorenci1@hotmail.com>
2025-02-23 07:39:13 -0500

    cart icon now shows how many items inside and fixed being able to decrement to negatitve numbers

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -31,7 +31,9 @@ const CartItem = ({ onContinueShopping, setAddedToCart }) => {
   };
 
   const handleDecrement = (item) => {
-    dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1}));
+    if (item.quantity > 1){
+        dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1}));
+    }
   };
 
   const handleRemove = (item) => {
